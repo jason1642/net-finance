b7859ed72d6ba9f60d3a87555328b13448c11287
jason1642 <jason1642@gmail.com>
2023-02-19 10:49:00 -0500

    Create TopMoversTable.tsx

---
+++ ClientApp/src/components/home-page/LeftColumn/TopMovers/TopMoversTable.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface ITopMoversTableProps {
+}
+
+const TopMoversTable: React.FunctionComponent<ITopMoversTableProps> = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+export default TopMoversTable;
