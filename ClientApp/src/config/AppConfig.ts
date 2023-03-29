d86244712fed3d1b86d2c519c29d76373a5bd5b8
jason1642 <jason1642@gmail.com>
2023-03-29 14:42:17 -0400

    web socket client - improved technique

---
+++ ClientApp/src/config/AppConfig.ts
@@ -0,0 +1,12 @@
+export const BaseConfig = {
+    apiUrl: 'https://localhost:7108',
+    wsUrl: 'wss://localhost:44465',
+    // socket has four state
+    webSocketState: {
+      CONNECTING: 0,
+      OPEN: 1,
+      CLOSING: 2,
+      CLOSED: 3,
+      NOTCONNECTED: 4, // I've added this so we can use it to understand that the connection hasn't started yet.
+    },
+  };
--- ClientApp/src/views/PublicChat.tsx
+++ ClientApp/src/views/PublicChat.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, } from 'react';
+import React, { useState, useEffect, useContext} from 'react';
 import styled from 'styled-components';
 import { getRoomMessages } from '../api-requests/chat-room-requests';
 import ChatDisplay from '../components/chat-room/ChatDisplay';
@@ -6,9 +6,7 @@ import ChatHeader from '../components/chat-room/Header';
 import UserInput from '../components/chat-room/UserInput';
 import { userApi } from "../redux/features/userApi";
 import useWebSocket, {ReadyState} from 'react-use-websocket';
-// import { SocketData, ClientToServerEvents, ServerToClientEvents, InterServerEvents } from '../types/socket-io-types';
-
-
+import {WebSocketContext} from '../context/PublicChatWebSocket'
 const Container = styled.div`
   display:flex;
   flex-direction: column;
@@ -24,10 +22,11 @@ const Container = styled.div`
 `;
 interface IPublicChatProps {
 }
-const WS_URL = 'wss://localhost:7108/chat/641ddeb20052e8bc2b1edd6a';
+// const WS_URL = 'wss://localhost:7108/';
 
 const PublicChat: React.FunctionComponent<IPublicChatProps> = ({}) => {
     // const socket: Socket<ServerToClientEvents, ClientToServerEvents>  = io('https://localhost:7108/chat', { transports: ["websocket"] } )
+    const { messageList } = useContext(WebSocketContext);
 
     const {data: userData} = userApi.endpoints.verifyUser.useQueryState()
     const [publicChatRoomData, setPublicChatRoomData]= React.useState<Array<any>>()
@@ -36,21 +35,21 @@ const PublicChat: React.FunctionComponent<IPublicChatProps> = ({}) => {
 
 
     
-      useEffect(() => {
-        const ws = new WebSocket(WS_URL)
-        ws.onopen = () => {
-            console.log('Connected to WebSocket server');
-          };
-          ws.onerror = () => {
-            console.log('ON ERROR EVENt');
-          };
-        setSocket(ws)
-        console.log(ws)
+    //   useEffect(() => {
+    //     const ws = new WebSocket(WS_URL)
+    //     ws.onopen = () => {
+    //         console.log('Connected to WebSocket server');
+    //       };
+    //       ws.onerror = () => {
+    //         console.log('ON ERROR EVENt');
+    //       };
+    //     setSocket(ws)
+    //     console.log(ws)
 
-      }, [])
-      useEffect(() => {
-        console.log(socket)
-      }, [socket]);   
+    //   }, [])
+    //   useEffect(() => {
+    //     console.log(socket)
+    //   }, [socket]);   
 
     // "undefined" means the URL will be computed from the `window.location` object
     // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:44465'
